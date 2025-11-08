9ee4cd6a360c642c549ed31aef1366d866fc71bf
Shark-off <S1193568198@outlook.com>
2025-11-08 16:24:51 +0800

    test

---
+++ app/auth/sign-in/login-form.tsx
@@ -0,0 +1,57 @@
+// app/auth/sign-in/login-form.tsx
+'use client'
+
+import HorizontalDividingLine from '@/components/shared/horizontal-dividing-line'
+import StarsBackground from '@/components/shared/stars-background'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { GithubIcon, RotateCcw } from 'lucide-react'
+import { signIn } from 'next-auth/react'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+
+export default function LoginForm() {
+  const search = useSearchParams()
+  const callbackUrl = search.get('callbackUrl') || '/admin'
+
+  return (
+    <div className="flex flex-col m-auto">
+      <Card className="relative w-[320px] rounded-3xl py-4 sm:w-full sm:min-w-[360px] sm:max-w-none">
+        <CardHeader className="flex justify-center items-center">
+          <CardTitle>
+            <h3>你要登录喵?</h3>
+          </CardTitle>
+          <CardDescription>暂时仅支持GitHub登录喵 OvO</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <main className="flex flex-col w-full gap-4">
+            <Button
+              type="button"
+              onClick={() => signIn('github', { callbackUrl })}
+              className="cursor-pointer"
+            >
+              <GithubIcon />
+              GitHub登录
+            </Button>
+
+            <HorizontalDividingLine />
+
+            <Link href="/">
+              <Button type="button" className="w-full cursor-pointer">
+                <RotateCcw />
+                回到过去喵~
+              </Button>
+            </Link>
+          </main>
+        </CardFooter>
+      </Card>
+      <StarsBackground />
+    </div>
+  )
+}
