9a644b268621c41f674d18b1207914950ef4d531
Shark-off <S1193568198@outlook.com>
2025-11-09 12:02:31 +0800

    test

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,11 +1,52 @@
-// app/auth/sign-in/page.tsx
-import { Suspense } from 'react'
-import LoginForm from './login-form'
+'use client'
 
-export default function SignInPage() {
+import HorizontalDividingLine from '@/components/shared/horizontal-dividing-line'
+import StarsBackground from '@/components/shared/stars-background'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { GithubIcon, RotateCcw } from 'lucide-react'
+import { signIn } from 'next-auth/react'
+import Link from 'next/link'
+
+export default function LoginPage() {
   return (
-    <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
-      <LoginForm />
-    </Suspense>
+    <div className="flex flex-col m-auto">
+      <Card className="relative w-[320px] rounded-3xl py-4 sm:w-full sm:min-w-[360px] sm:max-w-none">
+        <CardHeader className="flex justify-center items-center">
+          <CardTitle>
+            <h3>你要登录喵?</h3>
+          </CardTitle>
+          <CardDescription>暂时仅支持GitHub登录喵 OvO</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <main className="flex flex-col w-full gap-4">
+            <Button
+              type="button"
+              onClick={() => signIn('github', { redirectTo: '/admin' })}
+              className="cursor-pointer"
+            >
+              <GithubIcon />
+              GitHub登录
+            </Button>
+
+            <HorizontalDividingLine />
+
+            <Link href="/">
+              <Button type="button" className="w-full cursor-pointer">
+                <RotateCcw />
+                回到过去喵~
+              </Button>
+            </Link>
+          </main>
+        </CardFooter>
+      </Card>
+      <StarsBackground />
+    </div>
   )
 }
--- auth.ts
+++ auth.ts
@@ -1,29 +1,8 @@
-// src/auth.ts
 import NextAuth from 'next-auth'
-import Credentials from 'next-auth/providers/credentials'
 import GitHub from 'next-auth/providers/github'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GitHub,
-    Credentials({
-      name: 'credentials',
-      credentials: {
-        username: { label: '用户名', type: 'text' },
-        password: { label: '密码', type: 'password' },
-      },
-      async authorize(credentials) {
-        // 硬编码管理员，生产请换数据库查询
-        if (
-          credentials.username === 'sharkoff'
-          && credentials.password === '8198'
-        ) {
-          return { id: '1', name: 'sharkoff', email: '1193568198@outlook.com' }
-        }
-        return null
-      },
-    }),
-  ],
+  providers: [GitHub],
   pages: {
     signIn: '/auth/sign-in',
   },
@@ -33,9 +12,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       if (pathname.startsWith('/admin'))
         return !!auth
       return true
-    },
-    redirect({ url, baseUrl }) {
-      return url.startsWith('/') ? `${baseUrl}${url}` : `${baseUrl}/admin`
     },
   },
 })
